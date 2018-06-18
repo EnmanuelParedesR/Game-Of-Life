d81a4ea8bc39ecf0958ec7e122930e9fd8a9a2f9
Enmanuel paredes <EnmanuelParedesr@hotmail.com>
2018-06-18 01:22:48 -0400

    Game Of Life
    Made in console with a 'beatiful' design.

---
+++ Game Of Life/GameOfLife_Console.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace JuegoDeLaVida_Final._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Opcion;
+            int FilasI = 10;
+            int ColumnasI = 10;
+            int contador2 = 0;
+            List<Juego_Celulas> Generaciones = new List<Juego_Celulas>();
+            List<bool[,]> Generacion_Copia = new List<bool[,]>();
+            ConsoleKeyInfo Tecla;
+           
+            do
+            {
+                Console.Title = "Juego de la vida en c# | Enmanuel Paredes R 1071051";
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("\n");
+                Console.WriteLine("## Bienvenido al Juego de la Vida. ## \n " +
+               "Presione la opcion deseada:\n" +
+               "#1. Crear celulas aleatoriamente.\n" +
+               "#2. Introducir Celulas en la matrix.\n" +
+               "#3. Comenzar el juego en base a porcentaje de poblacion aleatoria \n" +
+               "#4. Cambiar el tamano de la pantalla de juego. \n" +
+               "#5. Salir del juego.\n");
+             
+                    Opcion = int.Parse(Console.ReadLine());
+
+                switch (Opcion)
+                {
+                    case 1:
+                        Console.ForegroundColor = ConsoleColor.DarkCyan;
+                        Generaciones.Add(new Juego_Celulas());
+                        Generaciones[contador2].Constructor(ColumnasI, FilasI);
+                        Generaciones[contador2].Llenado_Aleatorio();
+                        Cambio_Generaciones(contador2, Generaciones, Generacion_Copia, ColumnasI, FilasI);
+
+                        break;
+                    case 2:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Generaciones.Add(new Juego_Celulas());
+                        Generaciones[contador2].Constructor(ColumnasI, FilasI);
+
+                        do
+                        {
+
+                            int x, y;
+                            Console.WriteLine("Creacion de la Generacion #1 manualmente");
+                            Console.WriteLine("-----------------------------------------");
+                            Console.WriteLine("Introduzca la posicion en 'X': ");
+                            x = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Introduzca la posicion en 'Y': ");
+                            y = int.Parse(Console.ReadLine());
+
+                            Generaciones[contador2].Creacion_Manual(x, y);
+
+                            do
+                            {
+                                Console.WriteLine("Para seguir introduciendo celulas presione la flecha para bajar. Para salir presione Escape [Esc]");
+                                Tecla = Console.ReadKey(true);
+                                switch (Tecla.Key)
+                                {
+                                    case ConsoleKey.Escape:
+                                        Console.WriteLine("Ha salido correctamente!");
+                                        break;
+
+                                    case ConsoleKey.DownArrow:
+
+                                        break;
+
+                                    default:
+                                        Console.WriteLine("Opcion Erronea. Porfavor utilice la flecha para bajar para continuar o [Esc] para salir.");
+                                        Console.WriteLine("");
+                                        break;
+                                }
+
+                            } while (Tecla.Key != ConsoleKey.Escape && Tecla.Key != ConsoleKey.DownArrow);
+                          
+                        } while (Tecla.Key != ConsoleKey.Escape);
+
+                        Cambio_Generaciones(contador2, Generaciones, Generacion_Copia, ColumnasI, FilasI);
+                        break;
+
+                    case 3:
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        float porcentaje;
+
+                        while (true)
+                        {
+                            Console.WriteLine("Introduzca el porcentaje de poblacion que desea tener");
+
+                            porcentaje = int.Parse(Console.ReadLine());
+                            if (porcentaje > 100 || porcentaje < 0)
+                            {
+                                Console.WriteLine("Porcentaje invalido");
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        porcentaje = porcentaje / 100;
+                        Generaciones.Add(new Juego_Celulas());
+                        Generaciones[contador2].Constructor(ColumnasI, FilasI);
+                        Generaciones[contador2].Llena_Porcentaje(porcentaje);
+                        Cambio_Generaciones(contador2, Generaciones, Generacion_Copia, ColumnasI, FilasI);
+
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Introduzca la cantidad de columnas que tendra el juego");
+                        ColumnasI = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Introduzca la cantidad de filas que tendra el juego");
+                        FilasI = int.Parse(Console.ReadLine());
+
+                        break;
+
+                    case 5:
+                        Console.Clear();
+                        Console.WriteLine("Ha Salido correctamente!");
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("Opciòn invalida!");
+                        break;
+                }
+
+            } while (Opcion != 5);
+           
+        }
+
+        public static void Cambio_Generaciones(int contador2, List<Juego_Celulas> Generaciones, List<bool[,]> Generacion_Copia,int columna,int fila)
+        {
+            ConsoleKeyInfo Tecla1;
+          
+
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine("Generacion  #{0}", contador2 + 1);
+                Generaciones[contador2].Imprimir();
+
+                do
+                {
+                  
+                    Console.WriteLine("Presione la flecha de abajo para pasar a la siguiente generacion, presione Escape [Esc] salir del generador de generaciones.");
+
+                    Tecla1 = Console.ReadKey(true);
+
+                    switch (Tecla1.Key)
+                    {
+                        case ConsoleKey.Escape:
+                            
+                            if(contador2 -1 <= 0)
+                            {
+                                Console.WriteLine("Ha salido correctamente!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Ha salido correctamente!");
+                                Generacion_Copia = Generaciones[contador2 - 1].Matrices;
+                            }                       
+                            break;
+
+                        case ConsoleKey.DownArrow:
+                            
+                            break;                
+
+                        default:
+                            Console.WriteLine("Opcion Erronea. Porfavor utilice la flecha para bajar para continuar o [Esc] para salir.");
+                            Console.WriteLine("");
+                            break;
+                    }
+                } while (Tecla1.Key != ConsoleKey.Escape && Tecla1.Key != ConsoleKey.DownArrow);
+
+                contador2++;
+                Generaciones.Add(new Juego_Celulas());
+                Generaciones[contador2] = Generaciones[contador2 - 1];
+                Generaciones[contador2].Siguiente_Generacion();
+                Console.Beep();
+                Console.Clear();
+            } while (Tecla1.Key != ConsoleKey.Escape);
+                    
+            do
+            {
+                if (contador2 - 1 < 0)
+                {
+                    Console.WriteLine("Presione [Esc] para salir.");
+                }
+                else
+                {
+                    Console.WriteLine("Presione la flecha para arriba si desea ver alguna generacion, de lo contrario presione Escape [Esc]");
+                }
+                   
+                Tecla1 = Console.ReadKey(true);
+
+                switch (Tecla1.Key)
+                {
+                    case ConsoleKey.Escape:
+                        Console.WriteLine("Volviendo al menu principal...");
+                        Thread.Sleep(1500);
+                        break;
+
+                    case ConsoleKey.UpArrow:
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+
+                        if (contador2 - 1 < 0)
+                        {
+                            Console.WriteLine("ha llegado al final de las generaciones");
+                            Console.WriteLine(" ");
+                            
+                        }
+                        else
+                        {                            
+                            bool[,] Matrices_Buscadas = new bool[columna, fila];
+
+                            if(Generacion_Copia.Count == 0)
+                            {
+                                Console.WriteLine("No existe generacion anterior");
+                                break;
+                            }
+                            Matrices_Buscadas = Generacion_Copia[contador2 - 1];
+
+                            Console.WriteLine("Generacion #{0}", contador2);
+
+                            for (int x = 0; x < columna; x++)
+
+                            {
+                                for (int y = 0; y < fila; y++)
+
+                                    Console.Write("|{0}", Matrices_Buscadas[x, y] ? "■" : " ");
+
+                                Console.WriteLine("|");
+
+                            }                       
+                        }
+                        contador2--;
+                        Console.Beep();
+                        break;
+
+                    default:         
+                        
+                            Console.WriteLine("Opcion Erronea. Porfavor utilice la flecha para bajar para continuar o [Esc] para salir.");
+                            Console.WriteLine(" ");
+                     
+                        break;
+                }
+                
+            } while (Tecla1.Key != ConsoleKey.Escape);
+
+            Generaciones.Clear();
+        }
+    } 
+}
+
+
---
+++ Game Of Life/Juego_Celulas.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuegoDeLaVida_Final._2
+{
+    class Juego_Celulas
+    {
+     
+        public static int Columnas;
+        public static int Filas;
+        public bool[,] Matrix;
+        public List<bool[,]> Matrices = new List<bool[,]>();
+        public int Contador_Repeticiones = 0;
+
+        public void Constructor(int columnas, int filas)
+        {
+
+            Columnas = columnas;
+            Filas = filas;
+            Matrix = new bool[Columnas, Filas];
+        
+        }
+
+        public void Llenado_Aleatorio()
+        {
+
+            var Aleatorio = new Random();
+
+            for (int x = 0; x < Columnas; x++)
+            {
+
+                for (int y = 0; y < Filas; y++)
+                {
+
+                    this.Matrix[x, y] = Aleatorio.NextDouble() < 0.20;
+                }
+            }
+        }
+        public void Llena_Porcentaje(float porcentaje)
+        {
+
+            var Aleatorio = new Random();
+
+            for (int x = 0; x < Columnas; x++)
+            {
+
+                for (int y = 0; y < Filas; y++)
+                {
+
+                    this.Matrix[x, y] = Aleatorio.NextDouble() < porcentaje;
+                }
+            }
+        }
+
+        public void Siguiente_Generacion()
+        {
+            bool[,] Matrix_Secundaria = new bool[Columnas, Filas];
+            
+            if(Contador_Repeticiones == 0)
+            {
+                Matrices.Add(Matrix);      
+            }
+
+            for (int x = 0; x < Columnas; x++)
+            {
+
+                for (int y = 0; y < Filas; y++)
+                {
+
+                    Matrix_Secundaria[x, y] = this.GetNext(x, y);
+                }
+            }
+            Matrices.Add(Matrix_Secundaria);          
+            this.Matrix = Matrix_Secundaria;
+
+            Contador_Repeticiones++;
+
+        }
+
+        private bool GetNext(int x, int y)
+        {
+            var Viva = this.Matrix[x, y];
+            var Vivientes = this.Vecinos_Existentes(x, y);
+
+            if (!Viva)
+            {
+                return (Vivientes == 3);
+            }
+
+            return (Vivientes == 2 || Vivientes == 3);
+        }
+
+        private int Vecinos_Existentes(int x, int y)
+        {
+            var Celulas_Vecinas = this.Celulas_Vivas(x - 1, y - 1) + this.Celulas_Vivas(x + 0, y - 1) +
+
+                this.Celulas_Vivas(x + 1, y - 1) +
+
+                this.Celulas_Vivas(x - 1, y) +
+
+                this.Celulas_Vivas(x + 1, y) +
+
+                this.Celulas_Vivas(x - 1, y + 1) +
+
+                this.Celulas_Vivas(x + 0, y + 1) +
+
+                this.Celulas_Vivas(x + 1, y + 1);
+
+            return Celulas_Vecinas;
+
+        }
+
+        private int Celulas_Vivas(int x, int y)
+        {
+            if (x < 0 || x > Columnas - 1)
+            {
+                return 0;
+            }
+            if (y < 0 || y > Filas - 1)
+            {
+                return 0;
+            }
+            return this.Matrix[x, y] ? 1 : 0;
+        }
+
+        public void Imprimir()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            for (int x = 0; x < Columnas; x++)
+
+            {
+                for (int y = 0; y < Filas; y++)
+                     
+                    Console.Write("|{0}", this.Matrix[x, y] ? "■" : " ");
+
+                Console.WriteLine("|");
+
+            }
+          
+            Console.ForegroundColor = ConsoleColor.Blue;
+        }
+        
+        public void Creacion_Manual(int x, int y)
+        {
+            this.Matrix[x, y] = true;
+        }
+    }
+}
